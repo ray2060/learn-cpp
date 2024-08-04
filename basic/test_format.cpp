2c7ba751d216e55daac9034434405db3d4cf8ab5
Ray <60306877+ray2060@users.noreply.github.com>
2024-08-04 23:21:20 +0800

    Create test_format.cpp

---
+++ basic/test_format.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int alongname = 1;
+    int asn = 2;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        n--;
+    }
+    return 0;
+}
